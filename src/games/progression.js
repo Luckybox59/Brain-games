f69a8e1b4a9bd5f45926e69be1b3ff3a25d63566
Luckybox59 <Luckybox59@gmail.com>
2018-10-06 16:55:51 +0500

    fixed bugs in task7

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,21 +4,20 @@ import playGame from '../engine';
 
 const gameDescription = 'What number is missing in this progression?';
 
+const progressionLength = 10;
+const getProgression = (num, acc, count, step, emptyPos) => {
+  if (count > progressionLength) return acc;
+  const newAcc = `${acc} ${(count === emptyPos) ? '..' : num}`;
+  const newNum = num + step;
+  return getProgression(newNum, newAcc, count + 1, step, emptyPos);
+};
+
 const makeTask = () => {
   const step = getRandomNum(2, 5);
   const startNum = getRandomNum(1, 90);
-  const progressionLength = 10;
   const emptyPos = getRandomNum(1, progressionLength);
   const accumulator = '';
-
-  const getProgression = (num, acc, count) => {
-    if (count === progressionLength) return acc;
-    const newAcc = `${acc} ${(count === emptyPos) ? '..' : num}`;
-    const newNum = num + step;
-    return getProgression(newNum, newAcc, count + 1);
-  };
-
-  const userQuestion = getProgression(startNum, accumulator, 1);
+  const userQuestion = getProgression(startNum, accumulator, 1, step, emptyPos);
   const correctAnswer = String(startNum + (emptyPos - 1) * step);
   return cons(userQuestion, correctAnswer);
 };
