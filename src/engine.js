aa111b7f6b212d5dc5bff32f1607b3db26836627
Luckybox59 <Luckybox59@gmail.com>
2018-10-06 11:47:30 +0500

    remove func askName

--- src/engine.js
+++ src/engine.js
@@ -1,12 +1,11 @@
 import readlineSync from 'readline-sync';
 import { question, answer } from './pair';
 
-const askName = () => readlineSync.question('What is your name? ');
 const numOfQuestions = 3;
 
 const playGame = (gameRules, makeTask) => {
   console.log(`\nWelcome to the Brain Games!\n${gameRules}\n`);
-  const userName = askName();
+  const userName = readlineSync.question('What is your name? ');
   console.log(`Hello, ${userName}!\n`);
   for (let i = 0; i < numOfQuestions; i += 1) {
     const userTask = makeTask();
